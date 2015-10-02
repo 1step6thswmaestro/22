462c46f380ee10e083d1aeb85d7c4fe47f7708a4
Do Hoerin <hellodhlyn@gmail.com>
2015-10-02 16:25:36 +0900

    card design improved

--- src/frontend/todo/TaskItem.jsx
+++ src/frontend/todo/TaskItem.jsx
@@ -21,39 +21,44 @@ class TaskItem extends React.Component{
 		var descStr = this.props.task.description || '';
 		var rawMarkup = marked(descStr.toString(), {sanitize: true});
 		return (
-			<div className="card panel panel-default">
-				<div className="toggleview">
-					<input
-						className="toggle"
-						type="checkbox"
-						checked={this.props.task.timestampComplete!=""}
-						onChange={this.props.onToggle}
-						/>
+			<div className="panel panel-default">
+				<div className="panel-heading">
+					<h2 className="task-name">{this.props.task.name}</h2>
 				</div>
-				<div className="card-contents">
-					<div className="task-name">
-						<h2>{this.props.task.name}</h2>
+				<div className="panel-body">
+					<div className="row">
+						<div className="col-md-8">
+							<div className="task-description">
+								<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+							</div>
+						</div>
+						<div className="card-contents col-md-4">
+							<div className="task-importance">
+								중요도: {this.props.task.importance}
+							</div>
+							<div className="taskCreatedDate">
+								생성일: {this.getReadableDate(this.props.task.timestampCreated)}
+							</div>
+							<div className="taskStartedDate">
+								시작일: {this.getReadableDate(this.props.task.timestampStart)}
+							</div>
+							<div className="task-duedate">
+								마감일: {this.getReadableDate(this.props.task.timestampDuedate)}
+							</div>
+						</div>
 					</div>
-					<div className="task-importance">
-						IMPORTANCE: {this.props.task.importance}
-					</div>
-					<div className="task-duedate">
-						DUE: {this.getReadableDate(this.props.task.timestampDuedate)}
-					</div>
-					<div className="taskCreatedDate">
-						CREATED: {this.getReadableDate(this.props.task.timestampCreated)}
-					</div>
-					<div className="taskStartedDate">
-						STARTED: {this.getReadableDate(this.props.task.timestampStart)}
-					</div>
-					<div className="task-description">
-						<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-					</div>
-				</div>
-				<div className="card-control">
-					<div className="toolbar">
-						<button className="button postpone" label="Remined me later" onClick={this.props.onPostpone}/>
-						<button className="button discard" label="Discard this task" onClick={this.props.onDiscard} />
+					<div>
+						<div className="btn-group">
+							<button className="btn btn-check" onClick={this.props.onToggle}>
+								<span className="glyphicon glyphicon-check"></span> 완료
+							</button>
+							<button className="btn btn-default" label="Remind me later" onClick={this.props.onPostpone}>
+								<span className="glyphicon glyphicon-send"></span> 나중에 알림
+							</button>
+							<button className="btn btn-default" label="Discard this task" onClick={this.props.onDiscard}>
+								<span className="glyphicon glyphicon-trash"></span> 할 일 제거
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
