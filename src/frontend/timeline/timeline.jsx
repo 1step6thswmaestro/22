52dccb595d1c33661cc4541fce3b2480d433dcd8
inosphe <inosphe@gmail.com>
2015-10-16 20:53:58 +0900

    tlineline scrolling 구현중

--- src/frontend/timeline/timeline.jsx
+++ src/frontend/timeline/timeline.jsx
@@ -11,12 +11,71 @@ export default class Timeline extends React.Component{
 		super(props);
 		Object.assign(this, ViewBoxMixin);
 
+		this.state = {
+			tickInterval: {unit: 'hour', interval: 1}
+			, domainSize: 8
+			, leftOffset: 0
+		}
   	}
+
+	componentDidMount(){
+		let $el = React.findDOMNode(this.refs.background);
+		console.log('dom : ', $el);
+	    d3.select($el).call(this.makeDragBehavior());
+	}
+
+	makeDragBehavior(){
+  		let self = this;
+  		let scale;
+		let leftmost;
+		let origin;
+
+		return d3.behavior.drag()
+		// .origin(function(){
+		// 	//console.log('origin', this, this.getBoundingClientRect());
+		// 	return {x:0, y:0}
+		// })
+		.on('dragstart', function(d){
+			console.log('dragstart', d, d3.event);
+
+			d3.event.sourceEvent.stopPropagation();
+			scale = Object.assign({}, self.state.xScale);
+			leftmost = undefined;
+			origin = undefined;
+		
+		})
+		.on('drag', function(d){
+			let $el = React.findDOMNode(self.refs.background);
+			console.log({$el});
+			var cur = d3.mouse($el);
+
+			leftmost = leftmost || scale.invert(0);
+			origin = origin || scale.invert(cur[0]);
+			let cursor = scale.invert(cur[0]);
+
+
+			console.log(leftmost, cursor, origin, leftmost + (cursor-origin));
+
+			self.setState({leftCursor: new Date(cursor.getTime() + (leftmost.getTime()-origin.getTime()))});
+
+			//self.state.leftCursor = ;
+		})
+		.on('dragend', function(d){
+		})
+		;
+	}  	
 
 	render() {
 		var props = this.props;
-		var xAxisTickInterval = {unit: 'day', interval: 2}
 		console.log('props : ', props);
+
+		if(!this.props.logs || !this.props.logs.length){
+			console.log('this.props.logs : ', this.props.logs);
+			return (<g>
+				<rect ref='background' width='100%' height='100%' fill='#fff'>
+				</rect>
+			</g>)
+		}
 
 		var interpolationType = props.interpolationType || (props.interpolate ? 'cardinal' : 'linear');
 
@@ -35,21 +94,21 @@ export default class Timeline extends React.Component{
 
 		//var xValues = [new Date('2014-03-08T12:00:00.000Z'), new Date('2014-03-10T00:00:00.000Z')];
 		var xValues = _.map(this.props.logs, log=>new Date(log.time));
+		var domainWindow = d3.extent(xValues);
+		var domainSize = this.state.domainSize*60*60*1000;
+		var leftDomainWindow = [domainWindow[0], domainWindow[1]-domainSize];
+		var leftCursor = this.state.leftCursor || leftDomainWindow[0];
+		console.log({xValues, domainWindow, domainSize, leftDomainWindow, leftCursor})
+		console.log(leftCursor, leftCursor.getTime());
+		var domain = [new Date(leftCursor), new Date(leftCursor.getTime() + domainSize)];
 		var yMaxValues = [10];
 
-		var xScale;
-		console.log('xValues : ', xValues, Object.prototype.toString.call(xValues[0]), xAxisTickInterval);
-		if (xValues.length > 0 && Object.prototype.toString.call(xValues[0]) === '[object Date]' && xAxisTickInterval) {
-			xScale = d3.time.scale()
-				.range([0, innerWidth]);
-		} else {
-			xScale = d3.scale.linear()
-				.range([0, innerWidth]);
-		}
+		var xScale = this.state.xScale || d3.time.scale()
+		this.state.xScale = xScale;
+		xScale.range([0, innerWidth]);
 
-		console.log('extent : ', d3.extent(xValues));
-
-		xScale.domain(d3.extent(xValues));
+		xScale.domain(domain);
+		console.log('domain : ', domain);
 		// xScale.domain(xValues);
 		yScale.domain([0, d3.sum(yMaxValues)]);
 
@@ -63,11 +122,16 @@ export default class Timeline extends React.Component{
 		var trans = `translate(${ props.margins.left },${ props.margins.top })`;
 		return (
 			<g transform={trans} className={props.className}>
+				<rect ref='background' width='100%' height='100%' fill='#fff'>
+				</rect>
+				<text x="0" y="0" font-size="55">
+			    	{`${domain[0]}, ${domain[1]}`}
+			  	</text>
 				<XAxis
 					xAxisClassName='rd3-areachart-xaxis'
 					xScale={xScale}
 					xAxisTickValues={props.xAxisTickValues}
-					xAxisTickInterval={xAxisTickInterval}
+					xAxisTickInterval={this.state.tickInterval}
 					xAxisTickCount={props.xAxisTickCount}
 					xAxisLabel={props.xAxisLabel}
 					xAxisLabelOffset={props.xAxisLabelOffset}
