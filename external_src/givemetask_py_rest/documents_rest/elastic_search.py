273de62d36e81de11b67cfea3103eaa77936cff0
appleholic <cij1230@naver.com>
2015-11-02 17:16:51 +0900

    First commit filtering article docs in user feedly rss specified

--- external_src/givemetask_py_rest/documents_rest/elastic_search.py
+++ external_src/givemetask_py_rest/documents_rest/elastic_search.py
@@ -12,11 +12,8 @@ class DocumentES():
 
     # Task Call : {query : "", user_id : ""}
     def search(self, query, user_id, top_n=3):
-        rss_list = self.get_rss_list_by_user(user_id)
-        if rss_list is None:
-            return {}
         try:
-            es_res = self.es.search(index=self.index, body=self.get_search_body(query, rss_list))
+            es_res = self.es.search(index=self.index, body=self.get_search_body(query, user_id))
         except Exception, e:
             self.app.logger.info(e.message)
         finally:
@@ -25,19 +22,8 @@ class DocumentES():
                 return_object = {}
             return return_object
 
-    def get_rss_list_by_user(self, user_id):
-        return self.app.query_pool2.get_rss_list(user_id)
-
-    def get_search_body(self, query, rss_list):
-        result ={"query":
-            {"bool":
-                {"should": []}
-            }
-        }
-        print rss_list
-        feed_match = [{"match" : {"master_feed": item}} for item in rss_list]
-        result["query"]["bool"]["should"].append({"match" : {"summary" : query}})
-        result["query"]["bool"]["should"].extend(feed_match)
+    def get_search_body(self, query, user_id):
+        result ={"query" :  {"bool" : {"must" : [{"term" : {"userId" : user_id}},{"match" : {"summary" : query}}]}}}
         return result
 
     def res_to_json(self, res, top_n):
