abb8497007c1eebd8e85aeec3d366f66cad8aa63
appleholic <cij1230@naver.com>
2015-11-03 15:10:43 +0900

    Add Search Controller what determines the search numbers on elasticsearch or same clusters

--- external_src/givemetask_py_rest/apps/init.py
+++ external_src/givemetask_py_rest/apps/init.py
@@ -1,12 +1,12 @@
 __author__ = 'iljichoi'
 import logging
-from documents_rest.elastic_search import DocumentES
+from documents_rest.search_controller import SearchController
 from data_controller import db_tools
 
 # init all functions in this script
 def init_all(app):
     apply_logger(app)
-    app.es = init_elastic_search(app)
+    app.es = init_search_controller(app)
     app.query_pool = init_db("host")
     app.query_pool2 = init_db("host2")
 
@@ -17,8 +17,8 @@ def apply_logger(app):
     file_handler.setFormatter(formatter)
     app.logger.addHandler(file_handler)
 
-def init_elastic_search(app):
-    return DocumentES(app)
+def init_search_controller(app):
+    return SearchController(app)
 
 def init_db(select_host):
     pool = db_tools.Pool(select_host=select_host)
---
+++ external_src/givemetask_py_rest/documents_rest/search_controller.py
@@ -0,0 +1,42 @@
+from elastic_search import DocumentES
+from search_cluster import SearchCluster
+from config import NUMBER_DOCS
+
+class SearchController():
+    def __init__(self, app):
+        self.app = app
+        self.es = DocumentES(app)
+        self.cluster = SearchCluster(app)
+
+    def determine_numbers(self, user_id):
+        # logic to determine how many numbers to use in
+        # elasticsearch or same cluster.
+        user_article_count = self.app.query_pool2.get_article_count(user_id)
+
+        number = NUMBER_DOCS
+
+        if user_article_count < 100:
+            rss_number = 0
+            cluster_number = number
+        elif user_article_count < 500:
+            rss_number = number / 3
+            cluster_number = number / 3 * 2
+        else:
+            rss_number = number / 3 * 2
+            cluster_number = number / 3
+
+        return rss_number, cluster_number
+
+    def search(self, user_id, query):
+        rss_number, cluster_number = self.determine_numbers(user_id)
+        if rss_number > 0:
+            rss_list = self.es.search(query, user_id, rss_number)
+        else:
+            rss_list = []
+
+        if cluster_number > 0:
+            cluster_list = self.cluster.get_articles(user_id, query, cluster_number)
+        else:
+            cluster_list = []
+
+        return rss_list + cluster_list
