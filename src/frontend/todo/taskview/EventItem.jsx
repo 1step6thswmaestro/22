dd16afe57d054b9f897ce2626f1201ea9b04776c
inosphe <inosphe@gmail.com>
2015-11-06 21:39:48 +0900

    event item styling

--- src/frontend/todo/taskview/EventItem.jsx
+++ src/frontend/todo/taskview/EventItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import _ from 'underscore';
 import { getReadableDate } from '../../utility/date'
 import If from '../../utility/if'
+import TaskProgress from './TaskProgress'
 
 class EventItem extends React.Component{
 	constructor(props){
@@ -12,13 +13,22 @@ class EventItem extends React.Component{
 	getSimpleView(){
 		var event = this.props.event;
 		let start = moment(new Date(event.tableslotStart * (1000*60*30)));
+		let end = moment(new Date(event.tableslotEnd * (1000*60*30)));
+
+		console.log({event});
 
 		return (
-			<div className='task-item'>
-				{event.summary}
-				<div className='duedate'>
-					<i className='fa fa-clock-o'></i> {start.format("YY/MM/DD, HH:mm")}
+			<div className='table-item'>
+				<div className='table-item-header'>
 				</div>
+				<div className='table-item-header date'>
+					{start.format("HH:mm")}
+				</div>
+				<div className='table-item-header date date2 mr10'>
+					{end.format("HH:mm")}
+				</div>
+				{event.summary}
+				<TaskProgress count={event.estimation}/>
 			</div>
 		);
 	}
---
+++ src/frontend/todo/taskview/TaskProgress.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import _ from 'underscore';
+
+export default class TaskProgress extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		if(this.props.count<=0){
+			return <i></i>
+		}
+
+		let x = 0;
+		let contents = _.map(_.range(this.props.count), i=>{
+			let _x = x;
+			x += 5 + 3;
+			return (
+				<rect x={_x} className='progress-elem'>
+				</rect>
+			)
+		})
+
+		let width = this.props.count * 5 + (this.props.count-1) * 3
+		return (
+			<svg width={width} className='task-progress'>
+				{contents}
+			</svg>
+		);
+	}
+}
