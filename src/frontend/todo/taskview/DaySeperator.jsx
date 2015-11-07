9e2eb86f08c3f84ef90bd3eb345fde170bcbc99d
inosphe <inosphe@gmail.com>
2015-11-07 22:09:51 +0900

    Day Seperator 추가

---
+++ src/frontend/todo/taskview/DaySeperator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class DaySeperator extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		let momentTime = moment(this.props.time);
+
+		return (
+			<div className='day-seperator'>
+				{momentTime.format('MMM DD')}
+			</div>
+		)
+	}
+}
--- src/frontend/todo/taskview/TimeTable.jsx
+++ src/frontend/todo/taskview/TimeTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import EventItem from './EventItem'
 import _ from 'underscore'
+import moment from 'moment'
+import DaySeperator from './DaySeperator'
 
 export default class TimeTable extends React.Component{
 	constructor(props){
@@ -11,10 +13,23 @@ export default class TimeTable extends React.Component{
 		const { global, config, dispatch } = this.props;
     	var timetable = this.props.timetable;
 		var tasks = this.props.tasks;
+		var contents = [];
 
-		return _.map(timetable.list, item => (
-	        <EventItem key={item._id} event={item} task={tasks.tasks[item.taskId]} dispatch={dispatch} global={global} config={config}/>)
-		);
+		let latestDay = undefined;
+
+		_.each(timetable.list, item => {
+
+			let time = new Date(item.tableslotStart * (30 * 60 * 1000));
+			let day = time.getDay();
+			if(day != latestDay){
+				contents.push(<DaySeperator time={time} />)
+			}
+			
+			latestDay = day;
+			contents.push(<EventItem key={item._id} event={item} task={tasks.tasks[item.taskId]} dispatch={dispatch} global={global} config={config}/>);
+		});
+
+		return contents;
     }
 
 	render(){
