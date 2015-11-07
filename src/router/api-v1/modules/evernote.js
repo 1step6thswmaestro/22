dd998b77923ea31aedd0891f46fcdc04e7732da9
Insik Kim <insik92@gmail.com>
2015-11-08 01:57:05 +0900

    code cleanup for evernote integration.
    TODO:
    [ ] Change Evernote Sandbox -> Real Evernote
    [ ] Save every evernote to local 'Article' DB. Think again if we must save
    the notes because evernote already provide serach API.

--- src/router/api-v1/modules/evernote.js
+++ src/router/api-v1/modules/evernote.js
@@ -2,9 +2,7 @@
 
 let Q = require('q');
 let mongoose = require('mongoose');
-let Task = mongoose.model('Task');
 let express = require('express');
-
 
 module.exports = function(_router, app){
 	let helper = app.helper;
@@ -38,49 +36,42 @@ module.exports = function(_router, app){
 
 			// console.log(req.session.evernote);
 
-			res.redirect('/')
+			// Use below commented redirection after implementing '/pushtodb'.
+			res.redirect('/');
+			// res.redirect('/v1/evernote/pushtodb')
 		})
 		.fail((err)=>res.send(err.data));
 	})
 
-	router.get('/unauth', function(req, res){
-		app.helper.evernote.clearAuth(req.user)
-		.then(()=>res.send());
+	router.get('/pushtodb', function(req, res){
+		// Push every notes in the evernote account into Article DB.
+		// This function will be automatically callled during OAuth.
+		app.helper.evernote.getNotebookList(req.session.evernote.oauthAccessToken)
+		.then(notelist=>{
+			// LOOP OVER NOTEBOOK
+				// GET NOTES FROM NOTEBOOK
+				// LOOP OVER NOTES
+					// PUSH TO Article DB
+				// END LOOP
+			// END LOOP
+		})
+		.fail(err=>res.send(err));
 	})
 
-	router.get('/notebooklist/list', function(req, res){		
+	router.get('/unauth', function(req, res){
+		// Drop session data for Evernote.
+		req.session.evernote = {};
+	})
+
+	router.get('/notebooklist/list', function(req, res){
 		app.helper.evernote.getNotebookList(req.session.evernote.oauthAccessToken)
 		.then(results=>res.send(results))
 		.fail(err=>res.send(err));
 	})
 
-	function getStream(user, res, streamId){
-		app.helper.feedly.getStream(user, {count: 30, streamId})
-		.then(function(results){
-			results = JSON.parse(results);
-			res.send(results.items);
-		})
-		.fail(function(err){
-			res.status(400).send(err);
-		})
-	}
-
-	router.get('/stream/global', function(req, res){
-		let user = req.user;
-		let streamId = `user/${user.feedly.id}/category/global.all`
-		getStream(user, res, streamId);
-	})
-
-	router.get('/stream/:streamId?', function(req, res){
-		let user = req.user;
-		let streamId = req.params.streamId;
-		streamId = streamId || "user/2713debc-1be0-4773-b349-fe3955a50590/category/심심풀이 땅콩"
-
-		getStream(user, res, streamId);
-	})
-
 	router.get('/update', function(req, res){
-		app.helper.feedly.update(req.user)
+		// Update any changes of saved notes.
+		app.helper.evernote.update(req.user)
 		.then(function(results){
 			res.send(results);
 		})
