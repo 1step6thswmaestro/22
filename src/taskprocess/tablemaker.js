3b60b7fc185a4bc85ed77cb46e5cc7aad7ca3f31
Do Hoerin <hellodhlyn@gmail.com>
2015-11-16 03:15:08 +0900

    roll table maker ce57f42~1

--- src/taskprocess/tablemaker.js
+++ src/taskprocess/tablemaker.js
@@ -9,9 +9,8 @@ var TaskStateType = require('../constants/TaskStateType');
 const SLOT_SIZE = 1000*60*30;
 const HOUR_MILLISEC = 1000*60*60;
 const SLOT_NUMBER = 48*7;
-const SLOT_REVISE_NUMBER = 24*2*4;
-const TIMELEVEL_SIZE = 4; // hours
-const CALC_DAY_NUMBER = 5; // days
+const TIMELEVEL_SIZE = 4; //hours
+
 
 class TimeMaker{
 
@@ -52,27 +51,16 @@ class TimeMaker{
 		return _a.tableslotStart-_b.tableslotStart;
 	}
 
-	reviseTimeSlot(toRealtime, now, slot) {
-		if (toRealtime) {
-			let diff = slot - ((now + SLOT_REVISE_NUMBER) % SLOT_NUMBER);
-			if (diff < 0) diff += SLOT_NUMBER;
-			return now + diff;
-		}
-		else {
-			slot = (slot + SLOT_REVISE_NUMBER) % SLOT_NUMBER;
-		}
-		return slot;
-	}
+	
 
-	makeNewEvent(userId, taskId, _start, _end, _summary, _estimation, score) {
+	makeNewEvent(userId, taskId, _start, _end, _summary, _estimation) {
 		let newVal = {
 			userId,
 			taskId,
 			tableslotStart: _start, 
 			tableslotEnd: _end,
 			summary: _summary,
-			estimation: _estimation,
-			score
+			estimation: _estimation
 		}
 
 		return newVal;
@@ -80,6 +68,8 @@ class TimeMaker{
 
 	process(userId, originalTimeTables, events, tasks, opt) {
 		opt = opt || {};
+
+		console.log('process');
 
 		//methods & properties of this class
 		let helper = this.app.helper;
@@ -89,7 +79,6 @@ class TimeMaker{
 		let makeNewEvent = this.makeNewEvent;
 		let sortByTime = this.sortByTime;
 		let sortByTimelevel = this.sortByTimelevel;
-		let reviseTimeSlot = this.reviseTimeSlot;
 
 		//temporally used variables in this method.
 		let currentTime = opt.time || Date.now();
@@ -119,156 +108,120 @@ class TimeMaker{
 			});
 		}
 
-		function calculateIntervalScore(task) {
-			let intervalScore = [];
-			let loopSize = getTimeslot(task.duedate) - now - (task.adjustable?task.slotspan/2:task.slotspan);
-
-			// console.log(task.name, {loopSize});
-
-			let _loop = loopSize;
-			while (_loop-- > 0) {
-				let score = 0.0;
-				let length = task.adjustable?Math.min(loopSize-_loop, task.slotspan):task.slotspan;
-				let scoreIndex = reviseTimeSlot(false, now, now + _loop);
-
-				for (let i = 0; i <= length; i++) {
-					score += task.timePreferenceScore[(scoreIndex + i)%SLOT_NUMBER] || 0;
-				}
-
-				intervalScore.push({
-					from: scoreIndex
-					, to: scoreIndex + length
-					, score: score
-					, length: length
-				});
-			}
-
-			if(!intervalScore.length){
-				console.log(task.name, 'not allocated!');
-			}
-
-			if(task.name == 'sleep')
-				console.log(intervalScore);
-
-			intervalScore.sort(function(a, b){
-				if(a.score != b.score)
-					return b.score-a.score;
-
-				if(a.length != b.length)
-					return b.length-a.score;
-
-				if(a.from != b.from)
-					return a.from-b.from;
-				
-				if(a.to != b.to)
-					return b.to-a.to;
-
-				return 0;
-			});
-
-			if(task.name == 'sleep')
-				console.log(intervalScore);
-
-			return intervalScore;
-		}
-
 		function preprocessTasks(tasks){
 			return Q.all(tasks.map(task=>{
 				let p0 = helper.taskHelper.getProcessedTime(task, currentTime);
 				
 				return Q.spread([p0], (processedtime)=>{
 					task.timelevel = getTimeLevel(task, currentTime);
-					task.slotspan = Math.max(Math.floor(task.estimation * 2), 1);
-					task.intervalScore = calculateIntervalScore(task);
-					task.processedtime = Math.min(processedtime/HOUR_MILLISEC, 365*24);
-					
-					//console.log(task.name, task.intervalScore[0].score);
+					task.processedtime = processedtime;
 					return task;
 				})
 			}))
 		}
 
 		function processTasksByTimeLevel(tasks){
-			allocateTasks(slotAllocator, 0, 0);
-			console.log('table made score with: ', _MAX_SCORE);
+			_.each(tasks, processTaskByTimeLevel);
 		}
 
-		// Global variables for making timetable
-		var _TASKS;
-		var _MAX_SCORE = -1; // we are going to find this value
-		var _TIMETABLE = []; // this var is used during DFS search.
+		function processTaskByTimeLevel(task){
+			let level = task.timelevel;
+			let slot_begin = now%SLOT_NUMBER;
+			let slot_size = SLOT_NUMBER;
+			let timespan = Math.max(Math.floor(task.estimation*2 - task.processedtime),0);
+			timespan += (task.marginBefore||0) + (task.marginAfter||0);
 
-		function allocateTasks(allocator, score, indexCurrentTask) {
-			// score: score when we processed task up to task[indexCurrentTask-1].
-			var _task = _TASKS[indexCurrentTask];
-			var intervalScore = _task.intervalScore;
+			let timePreferenceScore = task.timePreferenceScore;
+			let slot_due = getTimeslot(task.duedate);
+			
+			if(slot_due-now < SLOT_NUMBER){
+				slot_size = slot_due - now;
+			}
 
-			// console.log(_task.name, intervalScore);
+			// console.log(task.name, task.adjustable, level, {now, slot_begin, slot_size, timespan});
+			// console.log(timePreferenceScore.length);
 
-			if (indexCurrentTask == _TASKS.length - 1) {
-				// Process the last tasks on the list.
-				// Special treatment on DFS terminal node. We don't want to waste stack resource.
+			if (task.adjustable || (0 <= level && level < (24*7/TIMELEVEL_SIZE))) {
+				// Calculate time prefer score for each slot term
+
+				let beginAfter = Math.floor((task.beginAfter || 0)/SLOT_SIZE); 
+
+				let scores = [];
+				for (let i = 0; i <= slot_size-(task.adjustable?1:timespan); ++i) {
+					let slot = now+i;
+
+					let start = slot;
+					let _timespan = timespan;
+
+					if(beginAfter>0)
+						console.log(task.name, beginAfter);
+
+					if(start < beginAfter){
+						continue;
+					}
+
+					if(slot_due - start < _timespan)	//only possible when 'adjustable' set
+						_timespan = slot_due-start;
+					
+					let end = slot + _timespan;
+					let allocation = slotAllocator.test(start, end, task.adjustable);
+					end = start + allocation;
+
+					if(allocation>0 || timespan==0){
+						let score = 0;
+						for (let j = 0; j < allocation; j++) {
+							score += timePreferenceScore[(start+j)%SLOT_NUMBER] || 0;
+						}
+						scores.push({start, end, score, length: allocation});
+					}
+				}
+
+				scores.sort(function(a, b){
+					if(a.score != b.score)
+						return b.score - a.score;
+
+					if(a.length != b.length)
+						return b.length - a.length;
+
+					if(a.start != b.start)
+						return a.start - b.start;
+					
+					if(a.end != b.end)
+						return b.end - a.end;
+
+					return 0;
+				});
+
+				if(task.name == 'sleep 2'){
+					console.log(scores);
+				}
+
+				// console.log('maximum score : ', _.max(scores, item=>item.score))
+				// console.log('minimum score : ', _.min(scores, item=>item.score))
 				
-				let i;
-				let allocation = 0;
-				for (i = 0; i < intervalScore.length; i++) {
-					let from = intervalScore[i].from;
-					let to = intervalScore[i].to;
+				// Checkout if the task's prefer slot is taken by the other
+				for (let i = 0; i < scores.length; i++) {
+					let scoreObj = scores[i];
+					let start = scoreObj.start;
+					let end = scoreObj.end;
+					let score = scoreObj.score;
+					let allocation = slotAllocator._alloc(start, end);
 
-					// console.log(_task.name, from, to);
-					allocation = allocator.test(from, to, _task.adjustable)
-					if (allocation > 0){
-						// console.log({from, to}, 'allocated.');
-						score += intervalScore[i].score;
-					    break;
+					if(allocation>0 || timespan==0){
+						let tableTask = makeNewEvent(userId, task._id, start+task.marginBefore||0, end-task.marginAfter||0, task.name, task.estimation);
+						if (tableTask) {
+							timetable.push(tableTask);
+							break;
+						}
 					}
-				}
-
-				// console.log(_task.name, {i, intervalScore: intervalScore.length, score, _MAX_SCORE}, intervalScore[i]);
-
-				if (score > _MAX_SCORE) {
-					_MAX_SCORE = score;
-					let start = reviseTimeSlot(true, now, intervalScore[i].from);
-					let _event = makeNewEvent(userId, _task.id, start, start+allocation, _task.name, 0, score)
-					_TIMETABLE.push(_event);
-
-					console.log('## MAX_SCORE ##', {score});
-					console.log(_.map(_TIMETABLE, item=>_.pick(item, 'summary', 'score')));
-
-					timetable = _.map(_TIMETABLE, _.clone);
-					slotAllocator = allocator;
-				}
-			}
-			else{
-				for (let i = 0; i < intervalScore.length; i++) {
-					let _interval = intervalScore[i];
-
-					// if unvisited interval
-					let from = _interval.from;
-					let to = _interval.to;
-					let allocation = allocator.test(from, to, _task.adjustable);
-					if (allocation > 0) {
-						// set visit
-						allocator._alloc(from, from+allocation);
-
-						let start = reviseTimeSlot(true, now, _interval.from);
-						let end = reviseTimeSlot(true, now, _interval.from+allocation);
-						let _event = makeNewEvent(userId, _task.id, start, end, _task.name, 0, _interval.score);
-						_TIMETABLE.push(_event);
-
-						console.log(`## ${indexCurrentTask} ##`);
-						console.log(i);
-						console.log(_.map(_TIMETABLE, item=>_.pick(item, 'summary', 'score')));
-						// get maximum score if allocate with this interval
-						allocateTasks(allocator.clone(), score + _interval.score, indexCurrentTask+1);
-						let popped = _TIMETABLE.pop();
-					}
-				}
+				}					
 			}
 		}
 
-		function processAndFilterPassedOrAlreadyPlayingTask(task){
+		function processAndFilterAlreadyPlayingTask(task){
 			let alreadyStarted = task.state==TaskStateType.named.start.id;
+			console.log(task.name, task._id, {alreadyStarted});
+			console.log(originalTimeTables);
 
 			if(alreadyStarted){
 				let query = {taskId: task._id};
@@ -278,9 +231,6 @@ class TimeMaker{
 				}
 				
 				timetable.push(timeTableEvent);
-				return false;
-			}
-			else if (getTimeslot(task.duedate) < now) {
 				return false;
 			}
 			else{
@@ -293,14 +243,15 @@ class TimeMaker{
 			return tasks.sort(sortByTimelevel);
 		})
 		.then(tasks=>{
-			tasks = _.filter(tasks, processAndFilterPassedOrAlreadyPlayingTask);
-
+			tasks = _.filter(tasks, processAndFilterAlreadyPlayingTask);
+			
 			//events
 			fillEvents(events);
 			
 			//tasks
-			_TASKS = tasks;
-			processTasksByTimeLevel(0, 0);
+			_.chain(tasks)
+			.groupBy('timelevel')
+			.each(processTasksByTimeLevel);
 
 			return timetable;
 		})
@@ -315,11 +266,11 @@ class TimeMaker{
 		return pOriginalTimeTable
 		.then(originalTimeTables=>_.map(originalTimeTables, item=>item.toObject()))
 		.then(originalTimeTables=>{
-			// console.log('originalTimeTables', originalTimeTables);
+			console.log('originalTimeTables', originalTimeTables);
 			// Now tablemaker remove all timetable for every requests.
 			let pReset = helper.timetable.reset(userId);
 			let pCal = helper.google.getCalendarEventsWeek(user, opt);
-			let pTask = helper.taskHelper.find(userId, {state : {$lt: 500}});
+			let pTask = helper.taskHelper.find(userId);
 			return Q.spread([pReset, pCal, pTask], (reset, events, tasks)=>{
 				return this.process(userId, originalTimeTables, events, tasks, opt);
 			})
